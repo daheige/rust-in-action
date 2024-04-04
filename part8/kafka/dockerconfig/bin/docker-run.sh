4f95f61be02394e4d85f6d5f059e0e005057ee34
daheige <zhuwei313@hotmail.com>
2024-04-04 21:01:38 +0800

    feat: update kafka in docker

--- part8/kafka/dockerconfig/bin/docker-run.sh
+++ part8/kafka/dockerconfig/bin/docker-run.sh
@@ -3,11 +3,5 @@ root_dir=$(cd "$(dirname "$0")"; cd ..; pwd)
 
 docker rm -f $(docker ps | grep kafka | awk '{print $1}')
 
-rm -rf $root_dir/{kafka,zookeeper}
-
-mkdir $root_dir/{kafka,zookeeper}
-chmod -R 777 $root_dir/kafka
-chmod -R 777 $root_dir/zookeeper
-
 cd $root_dir
 docker-compose up -d
