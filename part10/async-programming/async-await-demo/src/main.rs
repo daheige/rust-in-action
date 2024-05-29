e4a1b4be578fa3e703f4b233c6d62bafe69a38fa
daheige <zhuwei313@hotmail.com>
2024-05-29 23:36:03 +0800

    feat: async/await keywords

---
+++ part10/async-programming/async-await-demo/src/main.rs
@@ -0,0 +1,29 @@
+// 引入futures包提供的block_on执行器
+use futures::executor::block_on;
+async fn hello_cat() {
+    println!("hello, kitty!");
+}
+
+// 在async fn中调用Future不使用.await表达式
+async fn hello() {
+    println!("call hello_cat without .await keywords");
+    hello_cat();
+}
+
+// 在async fn中调用Future使用.await表达式
+async fn say() {
+    println!("call hello_cat with .await keywords");
+    hello_cat().await;
+    println!("hello,world");
+}
+
+fn main() {
+    // // 在hello函数中不使用.await表达式
+    // hello();
+
+    // 使用block_on阻塞的方式运行Future
+    // block_on(hello_cat());
+
+    let f = say();
+    block_on(f); // 使用执行器推进Future执行
+}
--- part10/async-programming/futures-demo/src/main.rs
+++ part10/async-programming/futures-demo/src/main.rs
@@ -9,26 +9,11 @@ async fn greet(name: &str) {
     println!("hello,{}!", name);
 }
 
-async fn hello_cat() {
-    println!("hello,kitty");
-}
-
-async fn say() {
-    // 在async fn函数中使用.await并不会阻塞当前线程，而是异步地等待Future完成。
-    hello_cat().await;
-}
-
 fn main() {
     println!("exec async task...");
-    // futures block_on阻塞当前线程，直到提供的Future运行完成
-    block_on(hello_world());
+    let future = hello_world(); // 返回结果是一个Future对象
+    block_on(future); // block_on阻塞当前线程，直到提供的Future运行完成
 
-    // 这里与block_on不同的是，say函数中的.await并不会阻塞当前的线程，
-    // 而是异步等待hello_cat函数返回的Future执行完成，
-    // 在等待的过程中，该线程还可以继续执行其它的Future，最终实现了并发处理的效果。
-    let f = say();
+    let f = greet("rust async programming");
     block_on(f);
-
-    block_on(greet("futures"));
-    block_on(greet("rust async programming"));
 }
