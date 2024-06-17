b8dcee82b860fd416961909a9d22c3ef289175fd
daheige <zhuwei313@hotmail.com>
2024-06-18 00:09:56 +0800

    feat: rust develop dockerfile

--- part10/qa-project/scripts/docker-cargo-build.sh
+++ part10/qa-project/scripts/docker-cargo-build.sh
@@ -2,5 +2,6 @@
 root_dir=$(cd "$(dirname "$0")"; cd ..; pwd)
 
 image_name=qa-project
+version=v1.0
 cd $root_dir
-docker build . -t $image_name
+docker build . -t $image_name:$version
--- part10/qa-project/scripts/docker-rust-dev.sh
+++ part10/qa-project/scripts/docker-rust-dev.sh
@@ -2,5 +2,6 @@
 root_dir=$(cd "$(dirname "$0")"; cd ..; pwd)
 
 image_name=qa-project-dev
+version=v1.0
 cd $root_dir
-docker build . -f Dockerfile-dev -t $image_name
+docker build . -f Dockerfile-dev -t $image_name:$version
