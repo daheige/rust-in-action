4a543b518c1fe35a93d96b584929c5553b97c929
daheige <zhuwei313@hotmail.com>
2024-09-07 23:09:25 +0800

    feat: upate part6 crontab shell

--- part6/date.sh
+++ part6/date.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
+
+# 定义日志文件路径，使用绝对路径
 log_file=$(cd "$(dirname "$0")";pwd)"/test.log"
-date >> $log_file
+
+# 输出当前时间
+current_time=`date +%Y-%m-%d\ %H:%M:%S`
+echo "current_time:"$current_time >> $log_file
+
 echo "exec ok"
 exit 0
