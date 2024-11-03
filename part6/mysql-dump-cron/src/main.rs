5bcf6db51d462cba25855d73902c9e39c0a53aae
daheige <zhuwei313@hotmail.com>
2024-11-03 21:18:07 +0800

    feat: update mysqldump

--- part6/mysql-dump-cron/src/main.rs
+++ part6/mysql-dump-cron/src/main.rs
@@ -1,11 +1,12 @@
 use chrono::Local;
 use dotenv::dotenv;
 use std::io::Write;
+use std::path::Path;
 use std::process::Command;
+use std::time::{Duration, SystemTime};
 use std::{env, fs};
 
-#[tokio::main]
-async fn main() {
+fn main() {
     // 读取.env配置文件
     dotenv().ok();
 
@@ -15,6 +16,19 @@ async fn main() {
     let db_host = env::var("MYSQL_HOST").expect("mysql host invalid");
     let db_port = env::var("MYSQL_PORT").expect("mysql host invalid");
     let db_name = env::var("MYSQL_DATABASE").expect("mysql host invalid");
+    let backup_dir = env::var("BACKUP_DIR").expect("backup_dir invalid");
+    let expired_days: u64 = env::var("EXPIRED_DAYS")
+        .expect("expired_days invalid")
+        .parse()
+        .unwrap_or(3);
+
+    // 创建备份目录
+    fs::create_dir_all(&backup_dir).expect("failed to create backup dir");
+
+    let res = clear_expired_backup_file(&backup_dir, expired_days);
+    if let Err(err) = res {
+        println!("failed to clear expired sql file,error:{}", err);
+    }
 
     // 定义mysqldump命令执行的参数选项
     let mut cmd = Command::new("mysqldump");
@@ -32,7 +46,6 @@ async fn main() {
 
     // 执行mysql备份命令
     let res = cmd.output();
-
     // 通过match关键字匹配命令执行结果
     match res {
         Err(err) => {
@@ -43,24 +56,59 @@ async fn main() {
                 // 定义备份文件名称
                 let fmt = "%Y%m%d%H%M%S";
                 let timestamp = Local::now().format(fmt).to_string();
-                let backup_path = format!("./{}_{}.sql", &db_name, timestamp);
+                let backup_file = format!("{}_{}.sql", &db_name, timestamp);
+                let backup_path = Path::new(&backup_dir).join(&backup_file);
 
                 // 创建数据库备份文件
                 let mut file =
                     fs::File::create(&backup_path).expect("failed to create mysql backup file");
 
-                // 将命令在标准输入中的结果结果写入到文件中
+                // 将命令执行结果写入到文件中
                 file.write_all(&output.stdout)
                     .expect("failed to write backup file");
 
                 // 这里可以根据实际情况发送邮件或短信通知...
-                mock_send_email(&db_name, &backup_path);
+                mock_send_email(&db_name, &backup_file);
             }
         }
     }
 }
 
-fn mock_send_email(db_name: &str, backup_path: &str) {
-    println!("backup database {} to {} success", db_name, backup_path);
+fn mock_send_email(db_name: &str, backup_file: &str) {
+    println!("backup database {} to {} success", db_name, backup_file);
     // 省略定义发送邮件的业务代码...
+}
+
+fn clear_expired_backup_file(dir: &str, expired_days: u64) -> anyhow::Result<()> {
+    // 指定目录路径
+    let path = Path::new(dir);
+    // 尝试读取目录中的文件，将过期的文件删除
+    let entries = fs::read_dir(path)?;
+    for entry in entries {
+        let path = entry?.path(); // 获取文件的路径
+
+        // 判断path是否是文件
+        if path.is_file() {
+            let ext = path.extension().unwrap();
+            if ext.ne("sql") {
+                println!("file:{:?} ext:{:?}", path, ext);
+                continue;
+            }
+
+            // 删除过期的备份文件
+            let metadata = path.metadata()?; // 获取文件的元信息
+            let created = metadata.created()?; // 文件创建时间
+            let now = SystemTime::now();
+            let interval = now.duration_since(created)?;
+            println!("interval:{:?}", interval);
+            if interval > Duration::from_secs(expired_days * 86400) {
+                println!("remove expired file:{:?} begin", path);
+                // 尝试删除文件，捕获并打印错误
+                fs::remove_file(&path)?;
+                println!("remove expired file:{:?} success", path);
+            }
+        }
+    }
+
+    Ok(())
 }
