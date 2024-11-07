629e3f6a0d0b92febbb24431019efee8569fef62
daheige <zhuwei313@hotmail.com>
2024-11-07 23:47:23 +0800

    feat: update part4

--- part4/hello-echo/src/main.rs
+++ part4/hello-echo/src/main.rs
@@ -1,6 +1,7 @@
 use tide::Request;
 use tide::log;
 
+// 指定async_std异步运行时
 #[async_std::main]
 async fn main() -> tide::Result<()>{
     // 启动日志组件
@@ -23,9 +24,8 @@ async fn main() -> tide::Result<()>{
     Ok(())
 }
 
-// 请求参数Request，并返回tide::Result
-// 由于不需要接收请求参数，所以这个Request提取数据为()空元组
+// 该路由处理器函数的请求参数是tide::Request类型，返回结果是tide::Result类型，
+// 由于不需要接收请求参数，所以这个Request提取数据为()空元组。
 async fn hello(_req: Request<()>)->tide::Result{
     Ok("hello,world".into())
 }
-
--- part4/short-url/src/handlers.rs
+++ part4/short-url/src/handlers.rs
@@ -86,7 +86,8 @@ pub async fn create_short_url(
     State(state): State<Arc<AppState>>,
     Json(payload): Json<ShortUrlRequest>,
 ) -> impl IntoResponse {
-    format!("request origin url:{}", payload.url);
+    println!("request origin url:{}", payload.url);
+
     // murmurhash算法生成出来的数字是u32类型的，重复的概率非常小
     // 如果有重复在实际业务中,可以在url后面追加随机字符串方式处理或者采用mysql唯一索引的方式处理
     let num = murmurhash32::murmurhash3(payload.url.as_bytes());
