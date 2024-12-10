6803ce147dc64e61228dd0773f7bdeb159c490eb
daheige <zhuwei313@hotmail.com>
2024-12-10 22:16:42 +0800

    feat: update part9

--- part9/string_utils/python-project/main.py
+++ part9/string_utils/python-project/main.py
@@ -1,12 +1,9 @@
 # 引入string_utils包
-import string_utils;
+import string_utils
+x = string_utils.sum_as_string(1, 2) # 调用string_utils包提供的函数
+print("sum_as_string(1,2) = ", x)
+s = string_utils.explode("a,b,c", ",")
+print("字符串a,b,c按照逗号分割后的列表是：", s)
+arr = s = string_utils.implode(['a', 'b', 'c'], ",")
+print("列表['a', 'b', 'c']按照逗号连接后的字符串为：", arr)
 
-# 调用string_utils包提供的函数
-x = string_utils.sum_as_string(1, 2);
-print("sum_as_string(1,2) = ", x);
-
-s = string_utils.explode("a,b,c", ",");
-print("字符串a,b,c按照逗号分割后的列表是：", s);
-
-arr = s = string_utils.implode(['a', 'b', 'c'], ",");
-print("列表['a', 'b', 'c']按照逗号连接后的字符串为：", arr);
